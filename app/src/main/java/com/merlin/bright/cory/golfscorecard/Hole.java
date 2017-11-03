cd792a5c041597d24e64093a4bcf3e4b8bfbd477
Cory <corysairplane@gmail.com>
2017-11-03 11:18:24 -0600

    Remove unused code

--- app/src/main/java/com/merlin/bright/cory/golfscorecard/Hole.java
+++ app/src/main/java/com/merlin/bright/cory/golfscorecard/Hole.java
@@ -7,12 +7,12 @@ package com.merlin.bright.cory.golfscorecard;
 public class Hole {
     private int mScore;
     private int mHole;
-    private String mHoleName ;
+    private String mHoleName;
 
     public Hole(int hole, int score) {
         mScore = score;
         mHole = hole;
-        mHoleName = "Hole "+mHole+": ";
+        mHoleName = "Hole " + mHole + ": ";
     }
 
     public int getScore() {
@@ -23,17 +23,12 @@ public class Hole {
         mScore = score;
     }
 
-    public void setHole(int hole) {
-        mHole = hole;
+    public int getHole() {
+        return mHole;
     }
-
-    public int getHole(){ return mHole;}
 
     public String getHoleName() {
         return mHoleName;
     }
 
-    public void setHoleName(int holeName) {
-        mHoleName = "Hole "+holeName+":";
-    }
 }
